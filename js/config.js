8ebf9408804e277355755f616dc2ee605bddb721
Luisba7u7 <82068605+Luisba7u7@users.noreply.github.com>
2026-05-14 02:13:03 +0000

    Deploying to gh-pages from @ Luisba7u7/Master-Dashboard@839d0bcfcc3f64a126719611498a4f358faf4b09 🚀

--- js/config.js
+++ js/config.js
@@ -1,15 +1,15 @@
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  MASTER_EMAIL: "",
+  apiKey: "AIzaSyAzt4bvZwfm4g-NJ0KKcYpV0SEjZevK3q4",
+  authDomain: "master-dashboard-4cc97.firebaseapp.com",
+  projectId: "master-dashboard-4cc97",
+  storageBucket: "master-dashboard-4cc97.firebasestorage.app",
+  messagingSenderId: "846272700314",
+  appId: "1:846272700314:web:9cd3fc0e7e61debec1a423",
+  MASTER_EMAIL: "ba6766208@gmail.com",
   EMAILJS: {
-    serviceID: "",
-    templateID: "",
-    publicKey: ""
+    serviceID: "service_l5p2gcx",
+    templateID: "template_1iif9ac",
+    publicKey: "cBT1WMyDK7UbM_GO1"
   }
 };
 window.FIREBASE_CONFIG = firebaseConfig;
